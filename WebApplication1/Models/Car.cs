d8f4392cd7e07b9de1530dbd864acb7a526b7cd7
lilvacuum <93257374+lilvacuum@users.noreply.github.com>
2022-11-21 13:15:05 +0100

    l

--- WebApplication1/Models/Car.cs
+++ WebApplication1/Models/Car.cs
@@ -10,16 +10,13 @@ namespace WebApplication1.Models
         public string Brand { get; set; }
         public string Model { get; set; }
         public string? Description { get; set; }
-        [Range(typeof()]
-        public int Year { get; set; }
+        [Range(1900,2023,ErrorMessage = "Please specify the cars model year")]
+        public String Year { get; set; }
         public double Mileage { get; set; }
         public CarType Type { get; set; }
         public FuelType FuelType { get; set; }
         public int Doors { get; set; }
-        if (true)
-	{
-
-	}
+    
         public double? FuelConsumption { get; set; }
         public double? ElectricityConsumption { get; set; }
         public int? HK { get; set; }
