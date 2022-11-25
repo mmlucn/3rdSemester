16401761120d7ee4abd5f9793238f6ed0ef517ca
lilvacuum <93257374+lilvacuum@users.noreply.github.com>
2022-11-25 11:14:04 +0100

    a

--- CarRentalSite/Pages/cars2/Index.cshtml.cs
+++ CarRentalSite/Pages/cars2/Index.cshtml.cs
@@ -7,25 +7,35 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using CarRentalSite.Data;
 using ModelsDap.Models;
+using Microsoft.AspNetCore.Identity;
+using CarRentalSite.Areas.Identity.Data;
 
 namespace CarRentalSite.Pages.cars2
 {
     public class IndexModel : PageModel
     {
-        HttpClient client = new HttpClient();
-
-        public IndexModel()
+        
+        private readonly UserManager<CarRentalSiteUser> _userManager;
+        private readonly IConfiguration _configuration;
+        public IndexModel(UserManager<CarRentalSiteUser> userManager, IConfiguration configuration)
         {
-            
+            _userManager = userManager;
+            _configuration = configuration;
         }
 
         public IList<Car> Car { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int ownerId)
         {
 
             HttpClient client = new HttpClient();
-            var res = await client.GetFromJsonAsync<List<Car>>(@"https://localhost:7124/api/Car/GetAllCars");
+            var user = await _userManager.GetUserAsync(User);
+           
+            var customer = await client.GetFromJsonAsync<Customer>($"https://localhost:7124/api/User?email={user.Email}");
+            ownerId = customer.Id;
+
+
+            var res = await client.GetFromJsonAsync<List<Car>>(@"https://localhost:7124/api/Car/GetAllUsersCars?id="+ ownerId);
             
             
 
