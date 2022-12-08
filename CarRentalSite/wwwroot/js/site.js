1205999f5c7b5c1e5a038ffffa28fb4f9b4072f6
lilvacuum <93257374+lilvacuum@users.noreply.github.com>
2022-12-08 18:16:17 +0100

    l

--- CarRentalSite/wwwroot/js/site.js
+++ CarRentalSite/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var container = document.getElementById("my-image-collection");
+var button = document.getElementById("next-button");
+var button1 = document.getElementById("previous-button");
+
+//button.style.display = 'none'
+
+//container.addEventListener('mouseenter', () => {
+//    // show the button when the mouse is over the container
+//    button.style.display = 'block';
+//});
+
+//container.addEventListener('mouseleave', () => {
+//    // hide the button when the mouse is no longer over the container
+//    button.style.display = 'none';
+//});
+
+button.addEventListener("click", function () {
+    container.scrollTop += 400;
+});
+button1.addEventListener("click", function () {
+    container.scrollTop -= 400;
+});
