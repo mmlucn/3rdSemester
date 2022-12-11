18d943ac08b8f6e2989020d8d240a37b25b546ae
lilvacuum <93257374+lilvacuum@users.noreply.github.com>
2022-12-11 17:41:09 +0100

    a

--- CarRentalSite/Pages/Index.cshtml.cs
+++ CarRentalSite/Pages/Index.cshtml.cs
@@ -1,19 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarRentalSite.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ModelsDap.Models;
 
 namespace CarRentalSite.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        
+
+        public Customer Customer { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            
         }
 
-        public void OnGet()
+        public async void OnGet()
         {
+            
+            
+
 
         }
     }
