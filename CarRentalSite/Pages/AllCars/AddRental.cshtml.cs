1a2fcdb40086d6369c150fe6b11e7ad0c06563f6
mmlucn <10407501@ucn.dk>
2022-12-18 21:36:22 +0100

    uy

--- CarRentalSite/Pages/AllCars/AddRental.cshtml.cs
+++ CarRentalSite/Pages/AllCars/AddRental.cshtml.cs
@@ -67,7 +67,7 @@ namespace CarRentalSite.Pages.AllCars
             //    var res = await _httpClient.PostAsJsonAsync<Rental>(@"api/Rental/AddRental", Rental);
             //}
 
-
+            //TODO: fix at modelstate ikke er valid.
 
 
             return RedirectToPage("./Index");
