bb03727ae3aac1cfef14c1e40eab4789c516a09f
mmlucn <10407501@ucn.dk>
2022-12-18 23:30:20 +0100

    dekstopclient

---
+++ DesktopClient/UsersForm.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace DesktopClient
+{
+    partial class UsersForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.firstNameTxt = new System.Windows.Forms.TextBox();
+            this.lastNameTxt = new System.Windows.Forms.TextBox();
+            this.addressTxt = new System.Windows.Forms.TextBox();
+            this.emailTxt = new System.Windows.Forms.TextBox();
+            this.phoneTxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // searchTxt
+            // 
+            this.searchTxt.Location = new System.Drawing.Point(88, 9);
+            this.searchTxt.Name = "searchTxt";
+            this.searchTxt.PlaceholderText = "Find user by email";
+            this.searchTxt.Size = new System.Drawing.Size(206, 23);
+            this.searchTxt.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Search user:";
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(300, 9);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(75, 23);
+            this.searchBtn.TabIndex = 2;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Firstname:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Lastname:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Address:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Email:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 15);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Phone:";
+            // 
+            // firstNameTxt
+            // 
+            this.firstNameTxt.Location = new System.Drawing.Point(80, 81);
+            this.firstNameTxt.Name = "firstNameTxt";
+            this.firstNameTxt.Size = new System.Drawing.Size(199, 23);
+            this.firstNameTxt.TabIndex = 8;
+            // 
+            // lastNameTxt
+            // 
+            this.lastNameTxt.Location = new System.Drawing.Point(80, 110);
+            this.lastNameTxt.Name = "lastNameTxt";
+            this.lastNameTxt.Size = new System.Drawing.Size(199, 23);
+            this.lastNameTxt.TabIndex = 9;
+            // 
+            // addressTxt
+            // 
+            this.addressTxt.Location = new System.Drawing.Point(80, 139);
+            this.addressTxt.Name = "addressTxt";
+            this.addressTxt.Size = new System.Drawing.Size(199, 23);
+            this.addressTxt.TabIndex = 10;
+            // 
+            // emailTxt
+            // 
+            this.emailTxt.Location = new System.Drawing.Point(80, 168);
+            this.emailTxt.Name = "emailTxt";
+            this.emailTxt.Size = new System.Drawing.Size(199, 23);
+            this.emailTxt.TabIndex = 11;
+            // 
+            // phoneTxt
+            // 
+            this.phoneTxt.Location = new System.Drawing.Point(80, 197);
+            this.phoneTxt.Name = "phoneTxt";
+            this.phoneTxt.Size = new System.Drawing.Size(199, 23);
+            this.phoneTxt.TabIndex = 12;
+            // 
+            // Users
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.phoneTxt);
+            this.Controls.Add(this.emailTxt);
+            this.Controls.Add(this.addressTxt);
+            this.Controls.Add(this.lastNameTxt);
+            this.Controls.Add(this.firstNameTxt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.searchTxt);
+            this.Name = "Users";
+            this.Text = "Users";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox searchTxt;
+        private Label label1;
+        private Button searchBtn;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox firstNameTxt;
+        private TextBox lastNameTxt;
+        private TextBox addressTxt;
+        private TextBox emailTxt;
+        private TextBox phoneTxt;
+    }
+}
---
+++ DesktopClient/UsersForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebApi;
+
+
+namespace DesktopClient
+{
+    public partial class UsersForm : Form
+    {
+
+        private Customer customer;
+        
+        public UsersForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void searchBtn_Click(object sender, EventArgs e)
+        {
+            ApiClient apiClient = new("", new HttpClient());
+            var res = await apiClient.UserAsync(@"");
+            if (res != null)
+                customer = res;
+        }
+    }
+}
